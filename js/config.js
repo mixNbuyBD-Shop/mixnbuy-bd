e7af44f626d586a17e87e53f4bc376f6aa7fe012
MixNbuy BD <mixnbuy.bd@gmail.com>
2026-09-17 10:48:59 +0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,15 @@
+window.MXB_CONFIG = {
+    SUPABASE_URL: "https://orecvjhywhauxmbcixhc.supabase.co",
+
+    SUPABASE_PUBLISHABLE_KEY:
+        "sb_publishable_CqMQX1IkKkT_QlwvWxAtSA_GKvam8ny",
+
+    SHOP_NAME: "MIXNBUY.BD",
+    CURRENCY: "BDT",
+
+    CREATE_ORDER_FUNCTION:
+        "https://orecvjhywhauxmbcixhc.supabase.co/functions/v1/create-order",
+
+    TRACK_ORDER_FUNCTION:
+        "https://orecvjhywhauxmbcixhc.supabase.co/functions/v1/track-order"
+};
